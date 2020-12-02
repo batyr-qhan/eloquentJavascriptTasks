bef76944eaef01bf1fa2f5649e3d43d4e2f72d26
Batyrkhan <batyrqh@protonmail.com>
2020-12-02 21:11:10 +0600

    my own loop

--- index.js
+++ index.js
@@ -8,3 +8,17 @@ const arr = arrays.reduce((resultArr, currItem) => {
   resultArr = resultArr.concat(currItem)
   return resultArr
 }, [])
+
+console.log(arr)
+
+// your own loop
+function loop(val, isInteger, decrement, log) {
+  let currVal = val
+
+  while(isInteger(currVal)) {
+    log(currVal)
+    currVal = decrement(currVal)
+  }
+}
+
+loop(3, n => n > 0, n => n - 1, console.log)
